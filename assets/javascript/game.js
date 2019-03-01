3f3145d42862c64439e805b83de8787f11ee16f6
Glacian22 <47679905+Glacian22@users.noreply.github.com>
2019-02-28 20:39:06 -0800

    pseudocode and game start func

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,37 @@
 var secretWord = "waffles";
+var userGuess;
+var guessesLeft = 10;
+var lettersGuessed = document.getElementById("letters-guessed");
 
-document.onkeyup = function(event) {
+var wordOnPage = {
+    secretWord: "waffles",
+    letters: []
+}
+
+function gameStart(){
+    //choose secret word, hardcoded right now
+
+    //draw in underscores equal to secret word
+    for(var i=0; i<wordsOnPage.secretWord.length; i++){
+        wordOnPage.letters[i] = "_";
+    }
+
+
+
+
+}
+
+
+gameStart();
+
+document.onkeyup = function (event) {
+
+    userGuess = event.key;
+
+    //add the most recent guess to the string lettersGuessed, write to page
+    lettersGuessed.textContent += userGuess;
+
+    //decrement guessesLeft and write to page
+    guessesLeft--;
+    document.getElementById("guesses-left").textContent = guessesLeft;
+}
